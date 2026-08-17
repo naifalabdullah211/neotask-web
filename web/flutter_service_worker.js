fb76d21710a1414a20c4c27818fff07f5ee9eda8
naifalabdullah211 <naifalabdullah211@gmail.com>
2026-08-17 23:02:01 +0300

    fix: use neutral service worker cleanup

---
+++ web/flutter_service_worker.js
@@ -0,0 +1,12 @@
+/* NEOTASK_SERVICE_WORKER_NEUTRAL_20260817_2 */
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => caches.delete(key)));
+    await self.clients.claim();
+    await self.registration.unregister().catch(() => false);
+  })());
+});
