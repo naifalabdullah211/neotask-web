aa152613578e8f826ad7f24f34a51ed07d64f9d5
naifalabdullah211 <naifalabdullah211@gmail.com>
2026-08-17 23:05:12 +0300

    fix: remove automatic startup navigation

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -6,7 +6,6 @@ self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
     const keys = await caches.keys();
     await Promise.all(keys.map((key) => caches.delete(key)));
-    await self.clients.claim();
     await self.registration.unregister().catch(() => false);
   })());
 });
